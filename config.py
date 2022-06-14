b6a2eac44f6cca3b9398b4103bb13e1b4889fc0f
Brandt Carlson <brandt.carlson11@gmail.com>
2022-06-14 14:36:50 -0500

    first

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__name__))
+
+
+class Config:
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
