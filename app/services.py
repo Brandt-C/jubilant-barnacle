215e36615b28a88e7448b40de1c462f3e74d82d3
Brandt Carlson <brandt.carlson11@gmail.com>
2022-06-15 15:44:12 -0500

    + move class

--- app/services.py
+++ app/services.py
@@ -50,3 +50,15 @@ class Pokedex:
     def show_dex(self):
         for n in self.pokemon:
             self.pokemon[n].disp_poke()
+
+class Move:
+    def __init__(self, name):
+        self.name = name
+        result = r.get('https://pokeapi.co/api/v2/move/' + self.name)
+        data = result.json()
+        self.id = data['id']
+        self.power = data['power']
+        self.pp = data['pp']
+        
+    def disp(self):
+        print(f"Name-{self.name}\tID-{self.id}\tPower-{self.power}\tPP-{self.pp}")
