189039184c73cfed61f46480844380736ba5b51e
Brandt Carlson <brandt.carlson11@gmail.com>
2022-07-29 14:57:21 -0500

    landing almost there

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,18 @@ from requests import request
 from app import app
 from flask import render_template, request
 from .forms import FindPoke
-from .models import Pokedex
+from .models import Pokedex, Pokemon, db
+from .services import choose4, funPhrase
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    x = db.session.query(Pokemon.name).limit(15).all()
+    rand_list = choose4(x)
+    funp = funPhrase()
+    randex = Pokedex()
+    for r in rand_list:
+        randex.add_poke(r)
+    return render_template('index.html', randex=randex, funp=funp)
 
 @app.route('/find-poke', methods=['GET', 'POST'])
 def find_poke():
--- app/services.py
+++ app/services.py
@@ -11,4 +11,27 @@
         
 #     def disp(self):
 #         print(f"Name-{self.name}\tID-{self.id}\tPower-{self.power}\tPP-{self.pp}")
+import time
+from random import choices, sample
 
+def choose4(arr):
+    time.sleep(0.25)
+    x = choices(arr, k=4)
+    z = list(map(lambda x: str(x)[2:-3], x))
+    return z
+
+def funPhrase():
+    x = [
+        "thirsts for blood!",
+        "is ready for battle.",
+        "has been waiting all their life for this. . .",
+        "drank lots of coffee this am and is ready to release some pent-up aggression!",
+        "will destroy thine foes.",
+        "- Please don't hurt me!",
+        "thinks your team looks puny and can't wait to destroy them.",
+        "will battle to the end of their hp.",
+        "smells blood.",
+        "will end you.",
+        "is almost out of bubblegum."
+    ]
+    return sample(x,4)
