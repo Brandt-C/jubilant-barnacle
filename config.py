7daed576ab866465d9be6b888cb567319dffac7e
Brandt Carlson <brandt.carlson11@gmail.com>
2022-11-11 13:05:08 -0600

    prepare deploy

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__name__))
 
 class Config:
     FLASK_APP = os.environ.get('FLASK_APP')
-    FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS =False
